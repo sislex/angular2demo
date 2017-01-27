05fe4c3295c3dee45d724f51e61052f55741b146
vITSaotome <vloseu@gmail.com>
2017-01-28 02:30:10 +0300

    Added saving list title and new item by pressing Enter key. Removed RouterLinkActive directive from navigation menu.

--- src/app/home/list/list.component.ts
+++ src/app/home/list/list.component.ts
@@ -8,19 +8,34 @@ import { Component, EventEmitter } from '@angular/core';
 
 export class ListComponent {
 
+  public isNewTitle = false;
+  public isNewListItem = false;
+  public listItem = '';
+  
   user_lists = [];
   u_list = {
     title: 'My list',
-    title_changed: false,
     items: []
   };
 
-  addListItem() {
-    this.u_list.items.push('My new item');
+  showNewListItemInput() {
+    this.isNewListItem = true;
+    setTimeout(function () {
+      this.setFocus();
+    }.bind(this), 0);
   }
 
-  changeListTitle() {
-    this.u_list.title_changed = !this.u_list.title_changed;
+
+  addNewListItem() {
+    if (this.listItem) {
+      this.u_list.items.push(this.listItem);
+      this.listItem = '';
+    }
+    this.isNewListItem = false;
+  }
+
+  changeTitle() {
+    this.isNewTitle = !this.isNewTitle;
     setTimeout(function () {
       this.setFocus();
     }.bind(this), 0);
