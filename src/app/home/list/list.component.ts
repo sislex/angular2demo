61e4272f55d4453d02a8f71056d9bcffbc2ceb04
vITSaotome <vloseu@gmail.com>
2017-01-28 13:14:03 +0300

    Replaced previous style for word wrapping for long words with a new one with hyphens support (note: chrome doesn't support hyphens). Added checks for empty title (old value will be used if a new one is empty) and new list item (empty item will not be added to list).

--- src/app/home/list/list.component.ts
+++ src/app/home/list/list.component.ts
@@ -8,37 +8,46 @@ import { Component, EventEmitter } from '@angular/core';
 
 export class ListComponent {
 
-  public isNewTitle = false;
-  public isNewListItem = false;
-  public listItem = '';
-  
-  user_lists = [];
-  u_list = {
+  private isNewTitle = false;
+  private isNewListItem = false;
+  private newListItem = '';
+  private currentListTitle = '';
+
+  // user_lists = [];
+
+  private u_list = {
     title: 'My list',
     items: []
   };
 
   showNewListItemInput() {
+    this.newListItem = '';
     this.isNewListItem = true;
     setTimeout(function () {
       this.setFocus();
     }.bind(this), 0);
   }
 
-
   addNewListItem() {
-    if (this.listItem) {
-      this.u_list.items.push(this.listItem);
-      this.listItem = '';
+    if (this.newListItem.trim()) {
+      this.u_list.items.push(this.newListItem);
     }
     this.isNewListItem = false;
   }
 
-  changeTitle() {
+  showUpdateTitleInput() {
+    this.currentListTitle = this.u_list.title;
     this.isNewTitle = !this.isNewTitle;
     setTimeout(function () {
       this.setFocus();
     }.bind(this), 0);
+  }
+  
+  updateTitle() {
+    if (!this.u_list.title.trim()) {
+      this.u_list.title = this.currentListTitle
+    }
+    this.isNewTitle = false;
   }
 
   public focusTrigger = new EventEmitter<boolean>();
